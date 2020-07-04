c582642859a0e6ec20a228806a8360c349e93668
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2020-07-04 17:04:40 -0400

    Giftmaking

---
+++ Testgift.py
@@ -0,0 +1,24 @@
+with open("Test Files/menusPortingShop.conf", "r", encoding="utf8") as File1:
+    file_contents = File1.read().split(sep="\n")
+with open("Test Files/menusPortingShopStacks.conf", "r", encoding="utf8") as File2:
+    file_contents2 = File2.read().split(sep="\n")
+gifts = []
+for thing in file_contents:
+    if "cost=" in thing and thing.split(":")[1] not in gifts:
+        gifts.append(thing.split(":")[1])
+for thing in file_contents2:
+    if "cost=" in thing and thing.split(":")[1] not in gifts:
+        gifts.append(thing.split(":")[1])
+for gift in gifts:
+    if gift[-1] == '"':
+        print(f'        {gift[:-1]} ' + "{")
+        print('            "0" {')
+        print(f'                reward="COMMAND:giftplayer %p% {gift[:-1]} 150"')
+        print('            }')
+        print('        }')
+    else:
+        print(f'        {gift} ' + "{")
+        print('            "0" {')
+        print(f'                reward="COMMAND:giftplayer %p% {gift} 150"')
+        print('            }')
+        print('        }')
